52125260b58dc5d3bf1f9975928ed4e76e5099fb
Daniel Rojas Jr <danny.rojas.jr@gmail.com>
2022-08-25 17:10:34 -0400

    Homework challenge2a completed

--- training/src/App.js
+++ training/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
+  const [name, setName] = React.useState("private!");
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. <br />
-          <b>Test run of application.</b>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+        <h1>Welcome, {name}</h1>
+        </div>
+        <div>
+        <input type="text" onChange={(event) => setName(event.target.value) } />
+        </div>
       </header>
     </div>
   );
